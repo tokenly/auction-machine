40a2ca32ec0256a9258e33da34a54959b90dfdb6
Devon Weller <dweller@devonweller.com>
2015-01-17 13:08:00 -0600

    quiet bower deployment

--- .deploy.sh
+++ .deploy.sh
@@ -2,7 +2,7 @@ echo; echo "updating dependencies";
 /usr/local/bin/composer.phar install -d lib --prefer-dist
 
 echo; echo "updating bower dependencies";
-$(cd public/public && bower install)
+$(cd public/public && bower -q install)
 
 echo; echo "clearing config";
 /bin/rm -v var/cache/app-config/*
