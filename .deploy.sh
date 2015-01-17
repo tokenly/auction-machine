0dca1a814c530778bf66b86fabe3e967b6a379d1
Devon Weller <dweller@devonweller.com>
2015-01-17 13:32:21 -0600

    update deploy config

--- .deploy.sh
+++ .deploy.sh
@@ -6,8 +6,8 @@ $(cd public/public && bower -q install)
 
 TARGET="var/cache/app-config"
 echo; echo "clearing config";
-if find "$TARGET" -mindepth 1 -print -quit | grep -q .; then
-    /bin/rm -v var/cache/app-config/*
+if [ -d "$TARGET" ] && [ "$(ls $TARGET)" ]; then
+    /bin/rm -v $TARGET/*
 else
     echo; echo "The directory $TARGET is empty or non-existent";
 fi
